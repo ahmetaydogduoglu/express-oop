115cda6d3c760c93f248697b0871aab6b19ff693
Ahmet Aydogduoglu <ahmet.aydogduoglu@path.com.tr>
2020-12-25 21:35:06 +0300

    removed node_modules

--- index.ts
+++ index.ts
@@ -1,11 +1,16 @@
 import express, { Router } from 'express';
 import { AuthController } from './src/controller';
+import bodyParser from 'body-parser';
 const port: number = 8080;
 const app = express();
 
+//use body-parser
+app.use(bodyParser.json());
+
+//routers
 const authController = new AuthController();
 app.use(authController.path, authController.setRoutes());
 
 app.listen(port, () => {
-    console.log(`listening ${port}`)
+    console.log(`listening ${port}`);
 })
